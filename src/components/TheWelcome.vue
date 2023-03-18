e63b852c9244442a1faba7961b1f074d399eb5e3
Abrar74774 <abrarshahriarhossain@gmail.com>
2023-03-18 22:00:02 +0300

    Change project name

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,7 +4,7 @@
 <template>
   <header class="hero h-screen bg-gray-800 text-white flex justify-center items-center">
     <div>
-      <h1 class="text-4xl font-bold lg:text-6xl">Welcome to Drawpad</h1>
+      <h1 class="text-4xl font-bold lg:text-6xl">Welcome to Canvasnote</h1>
       <h2 class="text-xl text-center mt-3">Your Personal Drawing and Markup Tool 📝</h2>
       <div class="mt-10 flex justify-center">
         <router-link to="/canvas">
